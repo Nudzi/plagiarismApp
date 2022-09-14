7d76506ac03b8348deaf68347496956a9857bf2d
Nudzi <nudzejma.kezo@gmail.com>
2022-09-14 23:41:39 +0200

    Add file opener

--- plagiarism.Mobile/plagiarism.Mobile/Global.cs
+++ plagiarism.Mobile/plagiarism.Mobile/Global.cs
@@ -7,5 +7,6 @@ namespace plagiarism.Mobile
         public static Users LoggedUser { get; set; }
         public static UsersPackageTypes UsersPackageType { get; set; }
         public static bool JustRegisterNoPackage { get; set; }
+        public static string OneDriveApiRoot { get; set; } = "https://api.onedrive.com/v1.0/";
     }
 }
--- plagiarism.Mobile/plagiarism.Mobile/Views/ScanDocPage.xaml.cs
+++ plagiarism.Mobile/plagiarism.Mobile/Views/ScanDocPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Plugin.FilePicker;
+﻿using Newtonsoft.Json.Linq;
+using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
 using Plugin.Permissions;
 using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,6 +15,7 @@ namespace plagiarism.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ScanDocPage : ContentPage
     {
+        private static FileData fileData = null;
         public ScanDocPage()
         {
             InitializeComponent();
@@ -34,6 +40,7 @@ namespace plagiarism.Mobile.Views
             return true;
         }
 
+
         [Obsolete]
         private async void Button_Clicked(object sender, EventArgs e)
         {
@@ -43,13 +50,13 @@ namespace plagiarism.Mobile.Views
 
                 if (requestAccessGrant)
                 {
-                    var filedata = await CrossFilePicker.Current.PickFile();
-                    if (filedata == null)
+                    fileData = await CrossFilePicker.Current.PickFile();
+                    if (fileData == null)
                         return;
 
                     //writing the filename to our view
                     //SupportingDocument is the x:name of our label in xaml
-                    SupportingDocument.Text = filedata.FileName;
+                    SupportingDocument.Text = fileData.FileName;
                 }
                 else
                 {
@@ -60,6 +67,35 @@ namespace plagiarism.Mobile.Views
             {
                 Console.WriteLine("Error occured ", ex);
             }
+        }
+
+        private void CheckText()
+        {
+            // Example #1
+            // Read the file as one string.
+            string text = System.IO.File.ReadAllText(fileData.FilePath, Encoding.UTF8);
+
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            // Example #2
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(fileData.FilePath, Encoding.UTF8);
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            CheckText();
         }
     }
 }
